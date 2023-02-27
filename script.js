672c8ef3cece5feeb84b1fb9a412c7f38b4e0ee1
SNEIDER <ledier.bustos.quipo@gmail.com>
2023-02-27 16:41:07 -0500

    Agregar primeros archivos del programa

---
+++ script.js
@@ -0,0 +1,13 @@
+const h1 = document.querySelector(`h1`);
+const input1 = document.querySelector(`#calculo1`);
+const input2 = document.querySelector(`#calculo2`);
+const btn = document.querySelector(`#btnCalculo`);
+const btnresult = document.querySelector(`#result`);
+btn.addEventListener(`click`, btnOnClick);
+
+
+function btnOnClick(){
+    const sumainput = Number(input1.value) + Number(input2.value);
+    btnresult.innerText = `El resultado es ` + sumainput;
+}
+h1.innerHTML = `Calculadora`;
